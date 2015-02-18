cf9162ff34c1ac3903adbaa7ff1aaaa1e0359f8e
Miguel Romero <miguel.romero@mail.escuelaing.edu.co>
2015-02-18 18:14:26 -0500

    Configuration file updated

--- downloads/pvslm-conf.py
+++ downloads/pvslm-conf.py
@@ -29,13 +29,13 @@ print copy.communicate("n\n")[0]
 
 for i in range (0,2):
 	replace=subprocess.Popen('sed -e "s,^PVSLM=.*$,PVSLM='+confPath+'," < pvslm.py > tmp.9996',shell=True)
-	output=subprocess.Popen('mv tmp.9996 pvslm',shell=True)
+	output=subprocess.Popen('mv tmp.9996 pvslm.py',shell=True)
 
 	replace=subprocess.Popen('sed -e "s,^PVSLMREP=.*$,PVSLMREP='+repoPath+'," < pvslm.py > tmp.9998',shell=True)
-	output=subprocess.Popen('mv tmp.9998 pvslm',shell=True)
+	output=subprocess.Popen('mv tmp.9998 pvslm.py',shell=True)
 
 	replace=subprocess.Popen('sed -e "s,^PVSLMSRC=.*$,PVSLMSRC='+srcPath+'," < pvslm.py > tmp.9997',shell=True)
-	output=subprocess.Popen('mv tmp.9997 pvslm',shell=True)
+	output=subprocess.Popen('mv tmp.9997 pvslm.py',shell=True)
 
-copy=subprocess.Popen('sudo cp -r pvslm '+confPath,shell=True)
+copy=subprocess.Popen('sudo cp -r pvslm.py '+confPath,shell=True)
 print copy.communicate("n\n")[0]
