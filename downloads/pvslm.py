f896c6548a7ebabe9a40e295c438a571ab0a30f0
Miguel Romero <miguel.romero@mail.escuelaing.edu.co>
2015-02-18 18:52:09 -0500

    Configuration file and pvslm updated

--- downloads/pvslm.py
+++ downloads/pvslm.py
@@ -1,27 +1,37 @@
 import argparse
 
-PVSLM=?
-PVSLMSRC=?
-PVSLMREP=?
+PVSLM='?'
+PVSLMSRC='?'
+PVSLMREP='?'
 
 def main():
 	
 	parser = argparse.ArgumentParser(version='PVS Library Manager 1.0',prog='PVS Library Manager')
 	subparsers = parser.add_subparsers(help='sub-command help')
 
-	# create the parser for the "a" command
-	pkg = subparsers.add_parser('pkg', help='Package manager help')
-	pkg.add_argument('bar', type=int, help='bar help')
-
-	# create the parser for the "b" command
+	# Create parser for the sources
 	src = subparsers.add_parser('src', help='Source manager help')
-	src.add_argument('--baz', choices='XYZ', help='baz help')
-
-	# parse some argument lists
-	#parser.parse_args(['a', '12'])
-	#Namespace(bar=12, foo=False)
-	#parser.parse_args(['--foo', 'b', '--baz', 'Z'])
-	#Namespace(baz='Z', foo=True)
+	# Optional arguments
+	src.add_argument("-a", "--add", action="store_true", help="Add a new source")
+	src.add_argument("-m", "--modify", action="store_true", help="Modify an existing source")
+	src.add_argument("-d", "--delete", action="store_true", help="Delete an existing source")
+	src.add_argument("-c", "--create", action="store_true", help="Create a new repository")
+	src.add_argument("-u", "--update", action="store_true", help="Update an existing repository")
+	src.add_argument("-r", "--remove", action="store_true", help="Remove an existing repository")
+	# Positional arguments
+	src.add_argument("name", type=str, help="Name of the repositoy or source")
+	src.add_argument("description", type=str, help="Descriprion of a source")
+	src.add_argument("url", type=str, help="URL of a source")
+	
+	# Create parser for the packages
+	pkg = subparsers.add_parser('pkg', help='Package manager help')
+	# Optional arguments
+	pkg.add_argument("-i", "--install", action="store_true", help="Install a package and all its dependecies")
+	pkg.add_argument("-u", "--update", action="store_true", help="Update a package and all its dependecies")
+	pkg.add_argument("-d", "--delete", action="store_true", help="Delete a package")
+	pkg.add_argument("-f", "--info", action="store_true", help="Get information about a package")
+	# Positional arguments
+	pkg.add_argument("name", type=str, help="Name of the package")
 	
 	args=parser.parse_args()
 	
