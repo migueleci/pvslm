4ccd619154996229a31b0ede5d1831a1ff7ded0d
Miguel Romero <miguel.romero@mail.escuelaing.edu.co>
2015-02-18 22:02:20 -0500

    Configuration file and pvslm updated

--- downloads/pvslm-conf.py
+++ downloads/pvslm-conf.py
@@ -24,25 +24,28 @@ srcPath=pathAssing('source')
 repoPath=pathAssing('repository')
 confPath=pathAssing('configuration')
 
-copy=subprocess.Popen('curl http://migueleci.github.io/pvslm/downloads/pvslm.py -o pvslm.py',shell=True)
-copy.communicate()[0]
+try:
+	copy=subprocess.Popen('curl http://migueleci.github.io/pvslm/downloads/pvslm.py -o pvslm.py',shell=True)
+	copy.communicate()[0]
 
-config='"'+confPath+'"'
-repoPath='"'+repoPath+'"'
-srcPath='"'+srcPath+'"'
+	config='"'+confPath+'"'
+	repoPath='"'+repoPath+'"'
+	srcPath='"'+srcPath+'"'
 
-for i in range (0,2):
-	replace=subprocess.Popen('sed -e "s,^PVSLM=.*$,PVSLM='+config+'," < pvslm.py > tmp.9996',shell=True)
-	output=subprocess.Popen('mv tmp.9996 pvslm.py',shell=True)
+	for i in range (0,2):
+		replace=subprocess.Popen('sed -e "s,pvslmPath,'+config+'," < pvslm.py > tmp.9996',shell=True)
+		output=subprocess.Popen('mv tmp.9996 pvslm.py',shell=True)
 
-	replace=subprocess.Popen('sed -e "s,^PVSLMREP=.*$,PVSLMREP="'+repoPath+'"," < pvslm.py > tmp.9998',shell=True)
-	output=subprocess.Popen('mv tmp.9998 pvslm.py',shell=True)
+		replace=subprocess.Popen('sed -e "s,pvslmRep,'+repoPath+'," < pvslm.py > tmp.9998',shell=True)
+		output=subprocess.Popen('mv tmp.9998 pvslm.py',shell=True)
 
-	replace=subprocess.Popen('sed -e "s,^PVSLMSRC=.*$,PVSLMSRC="'+srcPath+'"," < pvslm.py > tmp.9997',shell=True)
-	output=subprocess.Popen('mv tmp.9997 pvslm.py',shell=True)
+		replace=subprocess.Popen('sed -e "s,pvslmSrc,'+srcPath+'," < pvslm.py > tmp.9997',shell=True)
+		output=subprocess.Popen('mv tmp.9997 pvslm.py',shell=True)
 
-copy=subprocess.Popen('cp -r pvslm.py '+confPath,shell=True)
-copy=subprocess.Popen('chmod +x '+confPath+'/pvslm.py',shell=True)
-copy.communicate()[0]
+	copy=subprocess.Popen('cp -r pvslm.py '+confPath,shell=True)
+	copy=subprocess.Popen('chmod +x '+confPath+'/pvslm.py',shell=True)
+	copy.communicate()[0]
 
-print 'PVS Library Manager has been successfully configured. Thanks!'
+	print 'PVS Library Manager has been successfully configured. Thanks!'
+except:
+	print 'Something went wrong. Please try again.'
