c6ab064e5ed88a4d83c2e73335e47bc93bedb4d8
Miguel Romero <miguel.romero@mail.escuelaing.edu.co>
2015-02-19 05:51:44 -0500

    Configuration file and pvslm updated

--- downloads/pvslm.py
+++ downloads/pvslm.py
@@ -1,13 +1,23 @@
 import argparse
+import sys
+import subprocess
+import os
+from os import listdir
 
-PVSLM='pvslmPath'
-PVSLMSRC='pvslmSrc'
-PVSLMREP='pvslmRep'
+PVSLM='/home/miguel/Documents/pvslm/conf'
+PVSLMSRC='/home/miguel/Documents/pvslm/test'
+PVSLMREP='/home/miguel/Documents/pvslm/test'
+
+class PVSLMParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n\n' % message)
+        self.print_help()
+        sys.exit(2)
 
 def main():
-	
-	parser = argparse.ArgumentParser(version='PVS Library Manager 1.0',prog='PVS Library Manager')
-	subparsers = parser.add_subparsers(help='sub-command help')
+	#parser = argparse.ArgumentParser(version='PVS Library Manager 1.0',prog='PVS Library Manager')
+	parser = PVSLMParser(version='PVS Library Manager 1.0',prog='PVS Library Manager')
+	subparsers = parser.add_subparsers(help='sub-command help', dest='subparser_name')
 
 	# Create parser for the sources
 	src = subparsers.add_parser('src', help='Source manager help')
@@ -19,9 +29,9 @@ def main():
 	src.add_argument("-u", "--update", action="store_true", help="Update an existing repository")
 	src.add_argument("-r", "--remove", action="store_true", help="Remove an existing repository")
 	# Positional arguments
-	src.add_argument("name", type=str, help="Name of the repositoy or source")
-	src.add_argument("description", type=str, help="Descriprion of a source")
-	src.add_argument("url", type=str, help="URL of a source")
+	src.add_argument("name", type=str, help="Name of the repositoy or source") 
+	src.add_argument("description", type=str, help="Descriprion of a source",nargs='?') # Optional
+	src.add_argument("url", type=str, help="URL of a source",nargs='?') # Optional
 	
 	# Create parser for the packages
 	pkg = subparsers.add_parser('pkg', help='Package manager help')
@@ -33,7 +43,65 @@ def main():
 	# Positional arguments
 	pkg.add_argument("name", type=str, help="Name of the package")
 	
-	args=parser.parse_args()
+	args = parser.parse_args()
+	if args.subparser_name=='src':
+		if args.add:
+			if args.description!=None and args.url!=None:
+				name=args.name
+				desc=args.description
+				url=args.url
+				try:
+					files=listdir(PVSLMSRC)
+					sources =[None]
+					for f in files: 						
+						if os.path.splitext(f)[1]=='.list': 
+							sources.append(os.path.splitext(f)[0])
+					if name not in sources:
+						file_src=PVSLMSRC+'/'+name+'.list'
+						content='"URL = '+url+'\nName = '+name+'\nDescription = '+desc+'"'
+						crt=subprocess.Popen('sudo echo '+content+' > '+file_src,shell=True)
+						crt.communicate()[0]
+						print "Source "+name+" added successfully"
+					else:
+						print "The source already exist, try to modify it."
+				except :
+					print "Something went wrong. Please check the arguments and try again"
+			else:
+				print "Please include the description and url"
+		elif args.modify:
+			print 'Under development.'
+		elif args.delete:
+			name=args.name
+			try:
+				files=listdir(PVSLMSRC)
+				sources =[None]
+				for f in files: 
+					if os.path.splitext(f)[1]=='.list': 
+						sources.append(os.path.splitext(f)[0])
+				if name in sources:
+					crt=subprocess.Popen('sudo rm -rf '+PVSLMSRC+'/'+name+'.list',shell=True)
+					crt.communicate()[0]
+					print "Source "+name+" deleted successfully"
+				else:
+					print 'The source does not exist.'
+			except:
+				print "Something went wrong. Please check the arguments and try again"
+		elif args.create:
+			print 'Works'
+		elif args.update:
+			print 'Works'
+		elif args.remove:
+			print 'Under development.'
+	else :
+		if args.install:
+			print 'Under development.'
+		elif args.update:
+			print 'Under development.'
+		elif args.delete:
+			print 'Under development.'
+		elif args.info:
+			print 'Under development.'
 	
+		
 if __name__=='__main__':
 	main()	
