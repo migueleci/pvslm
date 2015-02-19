7ea9ecad8ce449a529305ac2d9bd5c92001fc2df
Miguel Romero <miguel.romero@mail.escuelaing.edu.co>
2015-02-18 21:42:00 -0500

    Configuration file and pvslm updated

--- downloads/pvslm-conf.py
+++ downloads/pvslm-conf.py
@@ -12,10 +12,10 @@ def pathAssing(name):
 	while(not ok):
 		try:
 			path=raw_input("Enter the "+name+" path: ")
-			if (os.path.exists(path)):
-				ok=True
-			else :
-				print 'The directory is not correct. Please try again!'
+			if (not os.path.exists(path)):
+				replace=subprocess.Popen('mkdir -p '+path,shell=True)	
+				replace.communicate()[0]
+			ok=True
 		except:
 			print 'The directory is not correct. Please try again!'
 	return path
@@ -25,10 +25,14 @@ repoPath=pathAssing('repository')
 confPath=pathAssing('configuration')
 
 copy=subprocess.Popen('curl http://migueleci.github.io/pvslm/downloads/pvslm.py -o pvslm.py',shell=True)
-print copy.communicate("n\n")[0]
+copy.communicate()[0]
+
+config='"'+confPath+'"'
+repoPath='"'+repoPath+'"'
+srcPath='"'+srcPath+'"'
 
 for i in range (0,2):
-	replace=subprocess.Popen('sed -e "s,^PVSLM=.*$,PVSLM="'+confPath+'"," < pvslm.py > tmp.9996',shell=True)
+	replace=subprocess.Popen('sed -e "s,^PVSLM=.*$,PVSLM='+config+'," < pvslm.py > tmp.9996',shell=True)
 	output=subprocess.Popen('mv tmp.9996 pvslm.py',shell=True)
 
 	replace=subprocess.Popen('sed -e "s,^PVSLMREP=.*$,PVSLMREP="'+repoPath+'"," < pvslm.py > tmp.9998',shell=True)
@@ -37,5 +41,8 @@ for i in range (0,2):
 	replace=subprocess.Popen('sed -e "s,^PVSLMSRC=.*$,PVSLMSRC="'+srcPath+'"," < pvslm.py > tmp.9997',shell=True)
 	output=subprocess.Popen('mv tmp.9997 pvslm.py',shell=True)
 
-copy=subprocess.Popen('sudo cp -r pvslm.py '+confPath,shell=True)
-print copy.communicate("n\n")[0]
+copy=subprocess.Popen('cp -r pvslm.py '+confPath,shell=True)
+copy=subprocess.Popen('chmod +x '+confPath+'/pvslm.py',shell=True)
+copy.communicate()[0]
+
+print 'PVS Library Manager has been successfully configured. Thanks!'
--- downloads/pvslm.py
+++ downloads/pvslm.py
@@ -1,8 +1,8 @@
 import argparse
 
-PVSLM='?'
-PVSLMSRC='?'
-PVSLMREP='?'
+PVSLM='pvslmPath'
+PVSLMSRC='pvslmSrc'
+PVSLMREP='pvslmRep'
 
 def main():
 	
