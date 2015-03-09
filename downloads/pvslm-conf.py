cbb8f8e86fb4de3fc5f9881e120aa53536c3297b
Miguel <miguel.romero@mail.escuelaing.edu.co>
2015-03-09 03:05:21 -0500

    Modification after first presentation

--- downloads/pvslm-conf.py
+++ downloads/pvslm-conf.py
@@ -15,8 +15,10 @@ for var in vals:
   if var.find(PVS)>=0:
     pos=var.find(PVS)
     low=var.rfind(":",0,pos)
-    high=var.find(":",pos)
-    PVSPATH=var[low+1:len(var)-high]
+    high=var.find(":",pos,len(var))
+    if high==-1:
+      high=len(var)
+    PVSPATH=var[low+1:high]
 
 if PVSPATH==None:
   print "PVS is not installed, please install and configure it first"
@@ -50,37 +52,55 @@ srcPath=pathAssing('repositories source configuration path',DEFAULT_REPOSRC_DIR)
 repoPath=pathAssing('repositories download path',DEFAULT_REPOS_DIR)
 
 try:
-	copy=subprocess.Popen('curl http://migueleci.github.io/pvslm/downloads/pvslm.py -o pvslm.py',shell=True)
-	copy.communicate()[0]
-
-	config='"'+confPath+'"'
-	repoPath='"'+repoPath+'"'
-	srcPath='"'+srcPath+'"'
-
-	for i in range (0,2):
-		replace=subprocess.Popen('sed -e "s,pvslmPath,'+config+'," < pvslm.py > tmp.9996',shell=True)
-		output=subprocess.Popen('mv tmp.9996 pvslm.py',shell=True)
-
-		replace=subprocess.Popen('sed -e "s,pvslmRep,'+repoPath+'," < pvslm.py > tmp.9998',shell=True)
-		output=subprocess.Popen('mv tmp.9998 pvslm.py',shell=True)
-
-		replace=subprocess.Popen('sed -e "s,pvslmSrc,'+srcPath+'," < pvslm.py > tmp.9997',shell=True)
-		output=subprocess.Popen('mv tmp.9997 pvslm.py',shell=True)
-
-	copy=subprocess.Popen('cp -r pvslm.py '+confPath,shell=True)
-	copy=subprocess.Popen('chmod +x '+confPath+'/pvslm.py',shell=True)
-	copy.communicate()[0]
-	
+  copy=subprocess.Popen('curl http://migueleci.github.io/pvslm/downloads/pvslm.py -o pvslm.py',shell=True)
+  copy.communicate()[0]
+  
+  config='"'+confPath+'"'
+  repoPath='"'+repoPath+'"'
+  srcPath='"'+srcPath+'"'
+  
+  replace=subprocess.Popen('sed -e "s,pvslmPath,'+config+'," < pvslm.py > tmp.9996',shell=True)
+  replace.communicate()[0]
+  
+  output=subprocess.Popen('mv tmp.9996 pvslm.py',shell=True)
+  output.communicate()[0]
+  
+  replace=subprocess.Popen('sed -e "s,pvslmRep,'+repoPath+'," < pvslm.py > tmp.9998',shell=True)
+  replace.communicate()[0]
+  
+  output=subprocess.Popen('mv tmp.9998 pvslm.py',shell=True)
+  output.communicate()[0]
+    
+  replace=subprocess.Popen('sed -e "s,pvslmSrc,'+srcPath+'," < pvslm.py > tmp.9997',shell=True)
+  replace.communicate()[0]
+  
+  output=subprocess.Popen('mv tmp.9997 pvslm.py',shell=True)
+  output.communicate()[0]
+    
+  copy=subprocess.Popen('cp -r pvslm.py '+confPath,shell=True)
+  copy.communicate()[0]
+  
+  copy=subprocess.Popen('chmod +x '+confPath+'/pvslm.py',shell=True)
+  copy.communicate()[0]
+  
   copy=subprocess.Popen('curl http://migueleci.github.io/pvslm/downloads/nasalib.list -o nasalib.list',shell=True)
+  copy.communicate()[0]
+  
   copy=subprocess.Popen('cp -r nasalib.list '+srcPath,shell=True)
   copy.communicate()[0]
   
   clone=subprocess.Popen('git clone https://github.com/nasa/pvslib.git '+repoPath+'/nasalib',shell=True)
   clone.communicate()[0]
   
-	delete=subprocess.Popen('rm -rf pvslm.py',shell=True)
+  delete=subprocess.Popen('rm -rf pvslm.py',shell=True)
+  delete.communicate()[0]
+  
+  delete=subprocess.Popen('rm -rf pvslm-install',shell=True)
+  delete.communicate()[0]
+  
   delete=subprocess.Popen('rm -rf nasalib.list',shell=True)
-	delete.communicate()[0]
-	print 'PVS Library Manager has been successfully configured. Thanks!'
+  delete.communicate()[0]
+  
+  print 'PVS Library Manager has been successfully configured. Thanks!'
 except:
-	print 'Something went wrong. Please try again.'
+  print 'Something went wrong. Please try again.'
--- downloads/pvslm.py
+++ downloads/pvslm.py
@@ -11,164 +11,163 @@ PVSLMSRC='pvslmSrc'
 PVSLMREP='pvslmRep'
 
 class PVSLMParser(argparse.ArgumentParser):
-    def error(self, message):
-        sys.stderr.write('error: %s\n\n' % message)
-        self.print_help()
-        sys.exit(2)
+  def error(self, message):
+    sys.stderr.write('error: %s\n\n' % message)
+    self.print_help()
+    sys.exit(2)
 
 def main():
-	#parser = argparse.ArgumentParser(version='PVS Library Manager 1.0',prog='PVS Library Manager')
-	parser = PVSLMParser(version='PVS Library Manager 1.0',prog='PVS Library Manager')
-	subparsers = parser.add_subparsers(help='sub-command help', dest='subparser_name')
+  #parser = argparse.ArgumentParser(version='PVS Library Manager 1.0',prog='PVS Library Manager')
+  parser = PVSLMParser(version='PVS Library Manager 1.0',prog='PVS Library Manager')
+  subparsers = parser.add_subparsers(help='sub-command help', dest='subparser_name')
+  
+  # Create parser for the sources
+  src = subparsers.add_parser('src', help='Source manager help')
+  # Optional arguments
+  src.add_argument("-a", "--add", action="store_true", help="Add a new source")
+  src.add_argument("-m", "--modify", action="store_true", help="Modify an existing source")
+  src.add_argument("-d", "--delete", action="store_true", help="Delete an existing source")
+  src.add_argument("-c", "--create", action="store_true", help="Create a new repository")
+  src.add_argument("-u", "--update", action="store_true", help="Update an existing repository")
+  src.add_argument("-r", "--remove", action="store_true", help="Remove an existing repository")
+  # Positional arguments
+  src.add_argument("name", type=str, help="Name of the repositoy or source") 
+  src.add_argument("description", type=str, help="Descriprion of a source",nargs='?') # Optional
+  src.add_argument("url", type=str, help="URL of a source",nargs='?') # Optional
+  
+  # Create parser for the packages
+  pkg = subparsers.add_parser('pkg', help='Package manager help')
+  # Optional arguments
+  pkg.add_argument("-i", "--install", action="store_true", help="Install a package and all its dependecies")
+  pkg.add_argument("-u", "--update", action="store_true", help="Update a package and all its dependecies")
+  pkg.add_argument("-d", "--delete", action="store_true", help="Delete a package")
+  pkg.add_argument("-f", "--info", action="store_true", help="Get information about a package")
+  # Positional arguments
+  pkg.add_argument("name", type=str, help="Name of the package")
+  
+  args = parser.parse_args()
+  if args.subparser_name=='src':
+    if args.add:
+      if args.description!=None and args.url!=None:
+        name=args.name
+        desc=args.description
+        url=args.url
+        try:
+          files=listdir(PVSLMSRC)
+          sources =[None]
+          for f in files: 						
+            if os.path.splitext(f)[1]=='.list': 
+              sources.append(os.path.splitext(f)[0])
+          if name not in sources:
+            file_src=PVSLMSRC+'/'+name+'.list'
+            content='"URL = '+url+'\nName = '+name+'\nDescription = '+desc+'\nEnable = Yes"'
+            crt=subprocess.Popen('echo '+content+' > '+file_src,shell=True)
+            crt.communicate()[0]
+            print "Source "+name+" added successfully"
+          else:
+            print "The source already exist, try to modify it."
+        except :
+          print "Something went wrong. Please check the arguments and try again"
+      else:
+        print "Please include the description and url"
+    elif args.modify:
+      print 'Under development.'
+    elif args.delete:
+      name=args.name
+      try:
+        files=listdir(PVSLMSRC)
+        sources =[None]
+        for f in files: 
+          if os.path.splitext(f)[1]=='.list': 
+            sources.append(os.path.splitext(f)[0])
+        if name in sources:
+          crt=subprocess.Popen('rm -rf '+PVSLMSRC+'/'+name+'.list',shell=True)
+          crt.communicate()[0]
+          print "Source "+name+" deleted successfully"
+        else:
+          print 'The source does not exist.'
+      except:
+        print "Something went wrong. Please check the arguments and try again"
+    elif args.create:
+      name=args.name
+      try:
+        files=listdir(PVSLMSRC)
+        sources =[None]
+        for f in files: 
+          if os.path.splitext(f)[1]=='.list': 
+            sources.append(os.path.splitext(f)[0])
+        if name in sources:
+          files=listdir(PVSLMREP)
+          repos =[None]
+          for f in files: 
+            if(os.path.isdir(PVSLMREP+'/'+f)): 
+              repos.append(f)
+          if name not in repos:
+            src=open(PVSLMSRC+'/'+name+'.list')
+            url=src.readline()
+            url=url[6:-1]
+            clone=subprocess.Popen('git clone '+url+' '+PVSLMREP+'/'+name,shell=True)
+            clone.communicate()[0]
+            print "Repository "+name+" created successfully"
+          else:
+            print "The repository "+name+" is already created, try to update it."
+        else:
+          print "The source your trying to create does not exist"
+      except:
+        print "Something went wrong. Please check the arguments and try again"
+    elif args.update:
+      name=args.name
+      try:
+        files=listdir(PVSLMSRC)
+        sources =[None]
+        for f in files: 
+          if os.path.splitext(f)[1]=='.list': 
+            sources.append(os.path.splitext(f)[0])
+        if name in sources:
+          files=listdir(PVSLMREP)
+          repos =[None]
+          for f in files: 
+            if(os.path.isdir(PVSLMREP+'/'+f)): 
+              repos.append(f)
+          if name in repos:
+            src=open(PVSLMSRC+'/'+name+'.list')
+            url=src.readline()
+            url=url[6:-1]
+            os.chdir(PVSLMREP+'/'+name)
+            clone=subprocess.Popen('git fetch '+url,shell=True)
+            clone.communicate()[0]
+            print "Repository "+name+" updated successfully"
+          else:
+            print "The repository "+name+" does not exists. Created it first"
+        else:
+          print "The source your trying to update does not exist"
+      except:
+        print "Something went wrong. Please check the arguments and try again"
+    elif args.remove:
+      name=args.name
+      try:
+        files=listdir(PVSLMREP)
+        repos =[None]
+        for f in files: 
+          if(os.path.isdir(PVSLMREP+'/'+f)): 
+            repos.append(f)
+        if name in repos:
+          clone=subprocess.Popen('rm -rf '+PVSLMREP+'/'+name,shell=True)
+          clone.communicate()[0]
+          print "Repository "+name+" removed successfully"
+        else:
+          print "The repository "+name+" does not exists. Created first"
+      except:
+        print "Something went wrong. Please check the arguments and try again"
+  else :
+    if args.install:
+      print 'Under development.'
+    elif args.update:
+      print 'Under development.'
+    elif args.delete:
+      print 'Under development.'
+    elif args.info:
+      print 'Under development.'
 
-	# Create parser for the sources
-	src = subparsers.add_parser('src', help='Source manager help')
-	# Optional arguments
-	src.add_argument("-a", "--add", action="store_true", help="Add a new source")
-	src.add_argument("-m", "--modify", action="store_true", help="Modify an existing source")
-	src.add_argument("-d", "--delete", action="store_true", help="Delete an existing source")
-	src.add_argument("-c", "--create", action="store_true", help="Create a new repository")
-	src.add_argument("-u", "--update", action="store_true", help="Update an existing repository")
-	src.add_argument("-r", "--remove", action="store_true", help="Remove an existing repository")
-	# Positional arguments
-	src.add_argument("name", type=str, help="Name of the repositoy or source") 
-	src.add_argument("description", type=str, help="Descriprion of a source",nargs='?') # Optional
-	src.add_argument("url", type=str, help="URL of a source",nargs='?') # Optional
-	
-	# Create parser for the packages
-	pkg = subparsers.add_parser('pkg', help='Package manager help')
-	# Optional arguments
-	pkg.add_argument("-i", "--install", action="store_true", help="Install a package and all its dependecies")
-	pkg.add_argument("-u", "--update", action="store_true", help="Update a package and all its dependecies")
-	pkg.add_argument("-d", "--delete", action="store_true", help="Delete a package")
-	pkg.add_argument("-f", "--info", action="store_true", help="Get information about a package")
-	# Positional arguments
-	pkg.add_argument("name", type=str, help="Name of the package")
-	
-	args = parser.parse_args()
-	if args.subparser_name=='src':
-		if args.add:
-			if args.description!=None and args.url!=None:
-				name=args.name
-				desc=args.description
-				url=args.url
-				try:
-					files=listdir(PVSLMSRC)
-					sources =[None]
-					for f in files: 						
-						if os.path.splitext(f)[1]=='.list': 
-							sources.append(os.path.splitext(f)[0])
-					if name not in sources:
-						file_src=PVSLMSRC+'/'+name+'.list'
-						content='"URL = '+url+'\nName = '+name+'\nDescription = '+desc+'"'
-						crt=subprocess.Popen('echo '+content+' > '+file_src,shell=True)
-						crt.communicate()[0]
-						print "Source "+name+" added successfully"
-					else:
-						print "The source already exist, try to modify it."
-				except :
-					print "Something went wrong. Please check the arguments and try again"
-			else:
-				print "Please include the description and url"
-		elif args.modify:
-			print 'Under development.'
-		elif args.delete:
-			name=args.name
-			try:
-				files=listdir(PVSLMSRC)
-				sources =[None]
-				for f in files: 
-					if os.path.splitext(f)[1]=='.list': 
-						sources.append(os.path.splitext(f)[0])
-				if name in sources:
-					crt=subprocess.Popen('rm -rf '+PVSLMSRC+'/'+name+'.list',shell=True)
-					crt.communicate()[0]
-					print "Source "+name+" deleted successfully"
-				else:
-					print 'The source does not exist.'
-			except:
-				print "Something went wrong. Please check the arguments and try again"
-		elif args.create:
-			name=args.name
-			try:
-				files=listdir(PVSLMSRC)
-				sources =[None]
-				for f in files: 
-					if os.path.splitext(f)[1]=='.list': 
-						sources.append(os.path.splitext(f)[0])
-				if name in sources:
-					files=listdir(PVSLMREP)
-					repos =[None]
-					for f in files: 
-						if(os.path.isdir(PVSLMREP+'/'+f)): 
-							repos.append(f)
-					if name not in repos:
-						src=open(PVSLMSRC+'/'+name+'.list')
-						url=src.readline()
-						url=url[6:-1]
-						clone=subprocess.Popen('git clone '+url+' '+PVSLMREP+'/'+name,shell=True)
-						clone.communicate()[0]
-						print "Repository "+name+" created successfully"
-					else:
-						print "The repository "+name+" is already created, try to update it."
-				else:
-					print "The source your trying to create does not exist"
-			except:
-				print "Something went wrong. Please check the arguments and try again"
-		elif args.update:
-			name=args.name
-			try:
-				files=listdir(PVSLMSRC)
-				sources =[None]
-				for f in files: 
-					if os.path.splitext(f)[1]=='.list': 
-						sources.append(os.path.splitext(f)[0])
-				if name in sources:
-					files=listdir(PVSLMREP)
-					repos =[None]
-					for f in files: 
-						if(os.path.isdir(PVSLMREP+'/'+f)): 
-							repos.append(f)
-					if name in repos:
-						src=open(PVSLMSRC+'/'+name+'.list')
-						url=src.readline()
-						url=url[6:-1]
-						os.chdir(PVSLMREP+'/'+name)
-						clone=subprocess.Popen('git fetch '+url,shell=True)
-						clone.communicate()[0]
-						print "Repository "+name+" updated successfully"
-					else:
-						print "The repository "+name+" does not exists. Created it first"
-				else:
-					print "The source your trying to update does not exist"
-			except:
-				print "Something went wrong. Please check the arguments and try again"
-		elif args.remove:
-			name=args.name
-			try:
-				files=listdir(PVSLMREP)
-				repos =[None]
-				for f in files: 
-					if(os.path.isdir(PVSLMREP+'/'+f)): 
-						repos.append(f)
-				if name in repos:
-					clone=subprocess.Popen('rm -rf '+PVSLMREP+'/'+name,shell=True)
-					clone.communicate()[0]
-					print "Repository "+name+" removed successfully"
-				else:
-					print "The repository "+name+" does not exists. Created first"
-			except:
-				print "Something went wrong. Please check the arguments and try again"
-	else :
-		if args.install:
-			print 'Under development.'
-		elif args.update:
-			print 'Under development.'
-		elif args.delete:
-			print 'Under development.'
-		elif args.info:
-			print 'Under development.'
-	
-		
 if __name__=='__main__':
-	main()	
+  main()	
