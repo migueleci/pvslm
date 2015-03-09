e1df1b5fcd31bed5ae0fa47c2082e30a113e9fbe
Miguel <miguel.romero@mail.escuelaing.edu.co>
2015-03-09 01:24:48 -0500

    Modification after first presentation

--- downloads/pvslm-conf.py
+++ downloads/pvslm-conf.py
@@ -1,15 +1,34 @@
 from sys import stdin
 from subprocess import check_output
+from fileinput import close
 import subprocess
+import sys
 import os
+
+global PVSPATH
+PVSPATH=None
+
+PVS="PVS"
+envVar=os.environ
+vals=envVar.values()
+for var in vals:
+  if var.find(PVS)>=0:
+    pos=var.find(PVS)
+    low=var.rfind(":",0,pos)
+    high=var.find(":",pos)
+    PVSPATH=var[low+1:len(var)-high]
+
+if PVSPATH==None:
+  print "PVS is not installed, please install and configure it first"
+  sys.exit()
 
 global repoPath
 global confPath
 global srcPath
 
-DEFAULT_INSTALL_DIR = '/usr/local/bin'
-DEFAULT_REPOSRC_DIR = '$HOME/.pvslm/reposrc'
-DEFAULT_REPOS_DIR = '$HOME/.pvslm/repos'
+DEFAULT_INSTALL_DIR = PVSPATH
+DEFAULT_REPOSRC_DIR = PVSPATH+'/.pvslm/reposrc'
+DEFAULT_REPOS_DIR = PVSPATH+'/.pvslm/repos'
 
 def pathAssing(name,default):
   ok=False
@@ -31,7 +50,7 @@ srcPath=pathAssing('repositories source configuration path',DEFAULT_REPOSRC_DIR)
 repoPath=pathAssing('repositories download path',DEFAULT_REPOS_DIR)
 
 try:
-	copy=subprocess.Popen('sudo curl http://migueleci.github.io/pvslm/downloads/pvslm.py -o pvslm.py',shell=True)
+	copy=subprocess.Popen('curl http://migueleci.github.io/pvslm/downloads/pvslm.py -o pvslm.py',shell=True)
 	copy.communicate()[0]
 
 	config='"'+confPath+'"'
@@ -39,20 +58,28 @@ try:
 	srcPath='"'+srcPath+'"'
 
 	for i in range (0,2):
-		replace=subprocess.Popen('sudo sed -e "s,pvslmPath,'+config+'," < pvslm.py > tmp.9996',shell=True)
+		replace=subprocess.Popen('sed -e "s,pvslmPath,'+config+'," < pvslm.py > tmp.9996',shell=True)
 		output=subprocess.Popen('mv tmp.9996 pvslm.py',shell=True)
 
-		replace=subprocess.Popen('sudo sed -e "s,pvslmRep,'+repoPath+'," < pvslm.py > tmp.9998',shell=True)
+		replace=subprocess.Popen('sed -e "s,pvslmRep,'+repoPath+'," < pvslm.py > tmp.9998',shell=True)
 		output=subprocess.Popen('mv tmp.9998 pvslm.py',shell=True)
 
-		replace=subprocess.Popen('sudo sed -e "s,pvslmSrc,'+srcPath+'," < pvslm.py > tmp.9997',shell=True)
+		replace=subprocess.Popen('sed -e "s,pvslmSrc,'+srcPath+'," < pvslm.py > tmp.9997',shell=True)
 		output=subprocess.Popen('mv tmp.9997 pvslm.py',shell=True)
 
-	copy=subprocess.Popen('sudo cp -r pvslm.py '+confPath,shell=True)
-	copy=subprocess.Popen('sudo chmod +x '+confPath+'/pvslm.py',shell=True)
+	copy=subprocess.Popen('cp -r pvslm.py '+confPath,shell=True)
+	copy=subprocess.Popen('chmod +x '+confPath+'/pvslm.py',shell=True)
 	copy.communicate()[0]
 	
-	delete=subprocess.Popen('sudo rm -rf pvslm.py',shell=True)
+  copy=subprocess.Popen('curl http://migueleci.github.io/pvslm/downloads/nasalib.list -o nasalib.list',shell=True)
+  copy=subprocess.Popen('cp -r nasalib.list '+srcPath,shell=True)
+  copy.communicate()[0]
+  
+  clone=subprocess.Popen('git clone https://github.com/nasa/pvslib.git '+repoPath+'/nasalib',shell=True)
+  clone.communicate()[0]
+  
+	delete=subprocess.Popen('rm -rf pvslm.py',shell=True)
+  delete=subprocess.Popen('rm -rf nasalib.list',shell=True)
 	delete.communicate()[0]
 	print 'PVS Library Manager has been successfully configured. Thanks!'
 except:
