bbbf4283a839707207e2b4c8e8442970f9e4b1ef
Miguel <miguel.romero@mail.escuelaing.edu.co>
2015-06-15 13:16:19 -0500

    refactoring

--- downloads/pvslm-conf.py
+++ downloads/pvslm-conf.py
@@ -110,6 +110,9 @@ try:
   copy=subprocess.Popen('rsync -azh '+repoPath+'/nasalib/install-scripts '+PVSPATH+'/nasalib/',shell=True)
   copy.communicate()[0]
   
+  install=subprocess.Popen('./'+PVSPATH+'/nasalib/install-scripts',shell=True)
+  install.communicate()[0]
+  
   delete=subprocess.Popen('rm -rf pvslm.py',shell=True)
   delete.communicate()[0]
   
