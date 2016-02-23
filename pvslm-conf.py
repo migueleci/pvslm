e52676732b01d9d8a4f2a2a0057352fbcd98cddb
Miguel Romero <miguel.romero@mail.escuelaing.edu.co>
2016-02-23 00:30:25 -0500

    NFM final rev

--- pvslm-conf.py
+++ pvslm-conf.py
@@ -8,6 +8,7 @@ import os
 global PVSPATH
 PVSPATH=None
 
+# Find PVS environment variable
 PVS="PVS_PATH"
 envVar=os.environ
 vals=envVar.keys()
@@ -16,6 +17,7 @@ for var in vals:
     PVSPATH=envVar[var]
     break
 
+# If the variable is not define, the abort the installation
 if PVSPATH==None:
   print "PVS is not installed, please install and configure it first"
   sys.exit()
@@ -27,10 +29,12 @@ global repoPath
 global confPath
 global srcPath
 
+# Define the paths for the installation
 DEFAULT_INSTALL_DIR = PVSPATH
 DEFAULT_REPOSRC_DIR = PVSPATH+'/.pvslm/reposrc'
 DEFAULT_REPOS_DIR = PVSPATH+'/.pvslm/repos'
 
+# User define Paths
 def pathAssing(name,default):
   ok=False
   while(not ok):
@@ -51,15 +55,18 @@ confPath=pathAssing('library manager installation path',DEFAULT_INSTALL_DIR)
 if confPath[-1]=='/':
   confPath=confPath[0:-1]
 
+# Create paths for the tool
+# Sources Path
 srcPath=confPath+'/.pvslm/reposrc' 
 replace=subprocess.Popen('mkdir -p '+srcPath,shell=True)
 replace.communicate()[0]
-
+# Git Repos Path
 repoPath=confPath+'/.pvslm/repos'
 replace=subprocess.Popen('mkdir -p '+repoPath,shell=True)
 replace.communicate()[0]
 
 try:
+  # Download pvslm tool
   copy=subprocess.Popen('curl http://migueleci.github.io/pvslm/downloads/pvslm.py -o pvslm.py',shell=True)
   copy.communicate()[0]
   
@@ -68,33 +75,39 @@ try:
   repoPath='"'+repoPath+'"'
   srcPath='"'+srcPath+'"'
   
+  # Modifiy tool with the especified paths (PVS)
   replace=subprocess.Popen('sed -e "s,pvsPath,'+pvsloc+'," < pvslm.py > tmp.9995',shell=True)
   replace.communicate()[0]
   
   output=subprocess.Popen('mv tmp.9995 pvslm.py',shell=True)
   output.communicate()[0]
 
+  # Modifiy tool with the especified paths (PVSlm)
   replace=subprocess.Popen('sed -e "s,pvslmPath,'+config+'," < pvslm.py > tmp.9996',shell=True)
   replace.communicate()[0]
   
   output=subprocess.Popen('mv tmp.9996 pvslm.py',shell=True)
   output.communicate()[0]
   
+  # Modifiy tool with the especified paths (PVSlm Repos)
   replace=subprocess.Popen('sed -e "s,pvslmRep,'+repoPath+'," < pvslm.py > tmp.9998',shell=True)
   replace.communicate()[0]
   
   output=subprocess.Popen('mv tmp.9998 pvslm.py',shell=True)
   output.communicate()[0]
     
+  # Modifiy tool with the especified paths (PVSlm Sources)
   replace=subprocess.Popen('sed -e "s,pvslmSrc,'+srcPath+'," < pvslm.py > tmp.9997',shell=True)
   replace.communicate()[0]
   
   output=subprocess.Popen('mv tmp.9997 pvslm.py',shell=True)
   output.communicate()[0]
-    
+  
+  # Save and copy tool to its path
   copy=subprocess.Popen('rsync -azh pvslm.py '+confPath,shell=True)
   copy.communicate()[0]
   
+  # Give permissions to the tool (executable)
   copy=subprocess.Popen('chmod +x '+confPath+'/pvslm.py',shell=True)
   copy.communicate()[0]
 
@@ -102,15 +115,19 @@ try:
     link=subprocess.Popen('ln -s '+confPath+'/pvslm.py '+PVSPATH+'/',shell=True)
     link.communicate()[0]
 
+  # Download NASALIB 
   copy=subprocess.Popen('curl http://migueleci.github.io/pvslm/downloads/nasalib.list -o nasalib.list',shell=True)
   copy.communicate()[0]
   
+  # Copy nasalib to the Sources Path
   copy=subprocess.Popen('rsync -azh nasalib.list '+srcPath,shell=True)
   copy.communicate()[0]
 
+  # Copy nasalib to the Repos Path
   clone=subprocess.Popen('git clone https://github.com/nasa/pvslib.git '+repoPath+'/nasalib',shell=True)
   clone.communicate()[0]
-    
+  
+  # Configure nasalib
   copy=subprocess.Popen('rsync -azh '+repoPath+'/nasalib/pvs-patches '+PVSPATH+'/nasalib/',shell=True)
   copy.communicate()[0]
 
@@ -121,6 +138,7 @@ try:
   install=subprocess.Popen('sh '+PVSPATH+'/nasalib/install-scripts',shell=True)
   install.communicate()[0]
   
+  # Remove installation files
   delete=subprocess.Popen('rm -rf pvslm-install',shell=True)
   delete.communicate()[0]
   
